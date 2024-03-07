795201d948c9ab1ebbcbc2262915955af7fc08d6
nimba005 <nimbaoyugi@gmail.com>
2024-03-07 20:26:04 +0300

    advance

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,12 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+}
