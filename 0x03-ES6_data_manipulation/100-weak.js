0e72f441949013969bb9d17a61fcd5320a705084
nimba005 <nimbaoyugi@gmail.com>
2024-03-13 07:06:10 +0300

    advance

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,7 @@
 const updateUniqueItems = (map) => {
   if (!(map instanceof Map)) throw new Error('Cannot process');
-
   map.forEach((value, key) => {
     if (value === 1) map.set(key, 100)
   });
 };
-export default updateUniqueItems;in
+export default updateUniqueItems;
